c2204db328ba561ad70e3ce10209ac9c612affb6
abbscbn <abbscbn@gmail.com>
2025-02-28 16:46:37 +0300

    Fix port type by converting process.env.PORT to number

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT || "10000", 10), // Render'ın kullanacağı port
+    host: "0.0.0.0", // Uygulamanızın dışarıdan erişilebilir olması için
+  },
+});
